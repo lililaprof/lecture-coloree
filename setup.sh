5d55c5a35ca966ef17798059762d5fdc06c01e8a
lililaprof <lisemarie.nicolai@gmail.com>
2025-12-05 09:53:20 +0100

    ;khj

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get update
 apt-get install -y tesseract-ocr libtesseract-dev tesseract-ocr-fra
-apt-get install -y libsm6 libxext6 libxrender-dev
+apt-get install -y libgl1-mesa-glx libglib2.0-0
