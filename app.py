c260dfefb02ec16d0ded8aed135af8e8d0f3307c
lililaprof <lisemarie.nicolai@gmail.com>
2025-12-20 15:09:11 +0100

    balise en python

--- app.py
+++ app.py
@@ -6,6 +6,21 @@ from docx.shared import RGBColor, Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 import io
 
+import streamlit.components.v1 as components
+
+# Google Analytics
+ga_code = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GKY6GERLTX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-GKY6GERLTX');
+</script>
+"""
+
+components.html(ga_code, height=0)
 # Configuration de base
 st.set_page_config(page_title="Lecture Colorée CP", page_icon="📚", layout="wide")
 
